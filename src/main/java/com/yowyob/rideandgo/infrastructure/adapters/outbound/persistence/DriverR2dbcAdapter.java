8f4c2b1528f9e11f9ab3cb677e4f4bf7cbf214a9
IgorGreenIGM <107557907+IgorGreenIGM@users.noreply.github.com>
2026-02-01 21:38:39 +0100

    Added reviewing features

--- src/main/java/com/yowyob/rideandgo/infrastructure/adapters/outbound/persistence/DriverR2dbcAdapter.java
+++ src/main/java/com/yowyob/rideandgo/infrastructure/adapters/outbound/persistence/DriverR2dbcAdapter.java
@@ -12,7 +12,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import java.util.UUID;
 
-@Slf4j
+@Slf4j 
 @Component
 @RequiredArgsConstructor
 public class DriverR2dbcAdapter implements DriverRepositoryPort {
